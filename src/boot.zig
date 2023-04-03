0455c68838c4b7890c83dc533f2cb727093bd2ef
ssstoyama <ssstoyama.develop@gmail.com>
2023-04-03 15:04:45 +0000

    improve load program segment

--- src/boot.zig
+++ src/boot.zig
@@ -2,10 +2,11 @@ const std = @import("std");
 const uefi = std.os.uefi;
 const elf = std.elf;
 const efi = @import("efi.zig");
+const util = @import("util.zig");
 
 pub fn main() void {
     efi_main() catch {
-        while (true) asm volatile ("hlt");
+        util.halt();
     };
 }
 fn efi_main() !void {
@@ -50,19 +51,13 @@ fn efi_main() !void {
     try efi.tryStatus(status);
     efi.printf("open kernel file\r\n", .{});
 
-    var file_info_size: usize = 0;
-    var file_info: uefi.protocols.FileInfo = undefined;
-    if (kernel_file.getInfo(&uefi.protocols.FileInfo.guid, &file_info_size, @ptrCast([*]u8, &file_info)) != .Success) {
-        status = kernel_file.getInfo(&uefi.protocols.FileInfo.guid, &file_info_size, @ptrCast([*]u8, &file_info));
-        try efi.tryStatus(status);
-    }
-    efi.printf("get kernel file info\r\n", .{});
+    var header_buffer: [*]align(8) u8 = undefined;
+    var header_size: usize = @sizeOf(elf.Elf64_Ehdr);
+    try readAndAllocate(kernel_file, &header_size, &header_buffer);
+    efi.printf("read header buffer\r\n", .{});
 
-    var kernel_buffer: [*]align(8) u8 = undefined;
-    try readAndAllocate(kernel_file, &file_info.file_size, &kernel_buffer);
-
-    const header = try elf.Header.parse(kernel_buffer[0..@sizeOf(elf.Elf64_Ehdr)]);
-    efi.printf("parse header from kernel file\r\n", .{});
+    const header = try elf.Header.parse(header_buffer[0..@sizeOf(elf.Elf64_Ehdr)]);
+    efi.printf("parse header from header buffer\r\n", .{});
 
     var kernel_first_addr: elf.Elf64_Addr align(4096) = std.math.maxInt(elf.Elf64_Addr);
     var kernel_last_addr: elf.Elf64_Addr = 0;
@@ -86,12 +81,20 @@ fn efi_main() !void {
     iter = header.program_header_iterator(kernel_file);
     while (try iter.next()) |phdr| {
         if (phdr.p_type != elf.PT_LOAD) continue;
-        loadProgramSegment(@ptrToInt(kernel_buffer), phdr);
+        try loadProgramSegment(kernel_file, phdr);
     }
     efi.printf("load program segment\r\n", .{});
 
     const entry_point = header.entry;
     efi.printf("kernel_entry_point=0x{x}\r\n", .{entry_point});
+
+    status = efi.bs.freePool(header_buffer);
+    try efi.tryStatus(status);
+    status = kernel_file.close();
+    try efi.tryStatus(status);
+    status = root_dir.close();
+    try efi.tryStatus(status);
+    efi.printf("free temporary resource\r\n", .{});
 
     efi.printf("exit boot service\r\n", .{});
     try exitBootServices();
@@ -112,15 +115,27 @@ fn exitBootServices() !void {
     try efi.tryStatus(status);
 }
 
-fn loadProgramSegment(base: usize, phdr: elf.Elf64_Phdr) void {
+fn loadProgramSegment(kernel_file: *uefi.protocols.FileProtocol, phdr: elf.Elf64_Phdr) !void {
     if (phdr.p_type != elf.PT_LOAD) return;
-    var dest: [*]u8 = @intToPtr([*]u8, phdr.p_vaddr);
-    var src: [*]u8 = @intToPtr([*]u8, base + phdr.p_offset);
-    efi.bs.copyMem(dest, src, phdr.p_filesz);
+
+    var status: uefi.Status = undefined;
+    var segment: [*]u8 = @intToPtr([*]u8, phdr.p_vaddr);
+    efi.printf("segment address: {*}\r\n", .{segment});
+
+    status = kernel_file.setPosition(phdr.p_offset);
+    try efi.tryStatus(status);
+    efi.printf("kernel file offset: 0x{x}\r\n", .{phdr.p_offset});
+
+    var mem_size: usize = phdr.p_memsz;
+    status = kernel_file.read(&mem_size, segment);
+    try efi.tryStatus(status);
+    efi.printf("read kernel file: size=0x{x}\r\n", .{mem_size});
+
     var zero_fill_count = phdr.p_memsz - phdr.p_filesz;
     if (zero_fill_count > 0) {
         efi.bs.setMem(@intToPtr([*]u8, phdr.p_vaddr + phdr.p_filesz), zero_fill_count, 0);
     }
+    efi.printf("zero fill count: 0x{x}\r\n", .{zero_fill_count});
 }
 
 fn readAndAllocate(file: *uefi.protocols.FileProtocol, size: *usize, buffer: *[*]align(8) u8) !void {
