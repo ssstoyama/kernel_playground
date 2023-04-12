5f9b417acc9dc8060be109452cb5dd2489ed9be5
ssstoyama <ssstoyama.develop@gmail.com>
2023-04-12 14:11:44 +0000

    add graphics.zig

---
+++ src/graphics.zig
@@ -0,0 +1,60 @@
+const FrameBufferConfig = @import("efi.zig").FrameBufferConfig;
+const PixelFormat = @import("efi.zig").PixelFormat;
+
+pub const PixelColor = struct {
+    r: u8 = 0,
+    g: u8 = 0,
+    b: u8 = 0,
+
+    pub fn black() PixelColor {
+        return PixelColor{ .r = 0, .g = 0, .b = 0 };
+    }
+
+    pub fn white() PixelColor {
+        return PixelColor{ .r = 255, .g = 255, .b = 255 };
+    }
+};
+
+pub const PixelWriter = union(enum) {
+    RGB: RGBPixelWriter,
+    BGR: BGRPixelWriter,
+
+    pub fn init(config: *const FrameBufferConfig) PixelWriter {
+        switch (config.pixel_format) {
+            PixelFormat.PixelRGBResv8BitPerColor => return PixelWriter{ .RGB = RGBPixelWriter{ .config = config } },
+            PixelFormat.PixelBGRResv8BitPerColor => return PixelWriter{ .BGR = BGRPixelWriter{ .config = config } },
+        }
+    }
+
+    pub fn offset(config: *const FrameBufferConfig, x: usize, y: usize) usize {
+        return (y * config.pixels_per_scan_line + x) * 4;
+    }
+
+    pub fn write(self: PixelWriter, x: usize, y: usize, color: PixelColor) void {
+        switch (self) {
+            inline else => |case| case.write(x, y, color),
+        }
+    }
+};
+
+const RGBPixelWriter = struct {
+    config: *const FrameBufferConfig,
+
+    pub fn write(self: RGBPixelWriter, x: usize, y: usize, color: PixelColor) void {
+        const offset = PixelWriter.offset(self.config, x, y);
+        self.config.frame_buffer[offset + 0] = color.r;
+        self.config.frame_buffer[offset + 1] = color.g;
+        self.config.frame_buffer[offset + 2] = color.b;
+    }
+};
+
+const BGRPixelWriter = struct {
+    config: *const FrameBufferConfig,
+
+    pub fn write(self: BGRPixelWriter, x: usize, y: usize, color: PixelColor) void {
+        const offset = PixelWriter.offset(self.config, x, y);
+        self.config.frame_buffer[offset + 0] = color.b;
+        self.config.frame_buffer[offset + 1] = color.g;
+        self.config.frame_buffer[offset + 2] = color.r;
+    }
+};
--- src/kernel.zig
+++ src/kernel.zig
@@ -1,23 +1,20 @@
 const BootInfo = @import("efi.zig").BootInfo;
 const util = @import("util.zig");
-const hello = @import("hello.zig");
+const graphics = @import("graphics.zig");
 
 export fn kernel_main(boot_info: *BootInfo) void {
     const frame_buffer_config = boot_info.frame_buffer_config;
+    const pixel_writer = graphics.PixelWriter.init(frame_buffer_config);
+
     {
         var x: usize = 0;
-        while (x < frame_buffer_config.horizontal_resolution) : (x += 1) {
+        while (x < 100) : (x += 1) {
             var y: usize = 0;
-            while (y < frame_buffer_config.vertical_resolution) : (y += 1) {
-                var p = @ptrCast([*]u8, &frame_buffer_config.frame_buffer[4 * (frame_buffer_config.pixels_per_scan_line * y + x)]);
-                p[0] = 255;
-                p[1] = 255;
-                p[2] = 255;
+            while (y < 100) : (y += 1) {
+                pixel_writer.write(x, y, graphics.PixelColor{ .r = 255, .b = 255 });
             }
         }
     }
-
-    hello.draw(boot_info.frame_buffer_config, 100, 100);
 
     util.halt();
 }
